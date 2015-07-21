894a77d31e458426c2b2379ed2f7197a628dfb52
Burhan Zainuddin <burhan@codeyellow.nl>
2015-07-21 22:49:26 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,39 @@
+# marionette-throttle
+
+Throttle methods! This is commonly used to disable multiple clicks on a button. 
+Each method which you want to throttle should return a promise. Every time a 
+method is invoked, an event is dispatched.  
+
+## Usage
+
+Define a view as follows:
+```js
+var Marionette = require('marionette'),
+    throttleBehavior = require('marionette-throttle');
+
+return Marionette.ItemView.extend({
+    behaviors: {
+        throttle: {
+            behaviorClass: throttleBehavior
+            methods: ['save']
+        }
+    },
+    events: {
+        // Method name inside event string.
+        throttle:save:enabled: 'enableSomeCssStuff',
+        throttle:save:disabled: 'disableSomeCssStuff',
+          
+        // Method name as an argument.
+        throttle:enabled: function (methodeName) {},
+        throttle:disabled: function (methodeName) {}
+    },
+    // This function will be wrapped because its name is present in
+    // the behavor `methods` config.
+    save: function (e) {
+        e.preventDefault();
+ 
+        return this.view.model.save();
+    }
+})
+
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+define(function (require) {
+    'use strict';
+
+    var Marionette = require('marionette');
+
+    return Marionette.Behavior.extend({
+        defaults: {
+            methods: []
+        },
+        isEnabled: {},
+        enable: function (method) {
+            this.isEnabled[method] = true;
+            this.view.triggerMethod('throttle:' + method + ':enabled');
+        },
+        disable: function (method) {
+            this.isEnabled[method] = false;
+            this.view.triggerMethod('throttle:' + method + ':disabled');
+        },
+        initialize: function () {
+            _.each(this.options.methods, function (method) {
+                // Enable all methods.
+                this.isEnabled[method] = true;
+
+                // Wrap original method.
+                this.view[method] = (function (original) {
+                    return function () {
+                        if (this.isEnabled[method]) {
+                            var xhr = original.apply(this.view, arguments);
+
+                            if (xhr && xhr.always) {
+                                this.disable(method);
+                                
+                                xhr.always(function () {
+                                    this.enable(method);
+                                }.bind(this));
+                            }
+
+                            return xhr;
+                        }
+                    }.bind(this);
+                }.bind(this)) (this.view[method]);
+            }.bind(this));
+        }
+    });
+});
